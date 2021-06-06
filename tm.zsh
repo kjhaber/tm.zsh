b501b6b41df4254c3c159ba7f346b59d7ab6934c
Keith Haber <kjhaber@gmail.com>
2021-06-05 23:19:33 -0700

    Add editorconfig
    Apply editorconfig rules to tm.zsh.  Using 2-space indent to follow
    ohmyzsh convention.

--- tm.zsh
+++ tm.zsh
@@ -5,8 +5,8 @@
 #
 
 function tm() {
-    [[ -z "$1" ]] && {
-      echo "Usage: tm <session>"
+  [[ -z "$1" ]] && {
+    echo "Usage: tm <session>"
       SESSIONS=$(tmux ls -F "* #{session_name}" 2>/dev/null)
       if [[ -n $SESSIONS ]]; then
         echo "Active tmux sessions:"
@@ -16,15 +16,15 @@ function tm() {
       fi
       return
     }
-    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-    tmux has -t="$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
+  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+  tmux has -t="$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
 }
 
 function __tmux-sessions() {
-    local expl
-    local -a sessions
-    sessions=( ${${(f)"$(command tmux list-sessions 2> /dev/null)"}/:[ $'\t']##/:} )
-    _describe -t sessions 'sessions' sessions "$@"
+  local expl
+  local -a sessions
+  sessions=( ${${(f)"$(command tmux list-sessions 2> /dev/null)"}/:[ $'\t']##/:} )
+  _describe -t sessions 'sessions' sessions "$@"
 }
 compdef __tmux-sessions tm
 
