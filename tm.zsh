77da65bd760e33f9388c30fd5aa98d4452b0e455
Keith Haber <kjhaber@gmail.com>
2023-11-04 14:32:39 -0700

    Attach to session with -u to ensure UTF-8, refactor tm script for clarity
    While testing my new dotfile config repo on Debian, I found that tmux
    wasn't using UTF-8 characters in my zsh prompt.  Add the -u argument
    when attaching to the session fixes it.
    
    Adding this arg was harder than it should have been.  I decided to
    replace boolean expression chaining with regular if-statements, and add
    some comments while I was at it.

--- tm.zsh
+++ tm.zsh
@@ -5,6 +5,7 @@
 #
 
 function tm() {
+  # If no session name arg provided, display usage and list active sessions
   [[ -z "$1" ]] && {
     echo "Usage: tm <session>"
       SESSIONS=$(tmux ls -F "* #{session_name}" 2>/dev/null)
@@ -16,8 +17,27 @@ function tm() {
       fi
       return
     }
-  [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-  tmux has -t="$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
+
+  # Either switch client or attach session depending on whether command
+  # is run from within a tmux session
+  if [[ -n "$TMUX" ]]; then
+    change="switch-client"
+  else
+    change="attach-session"
+  fi
+
+  # Determine whether a session with the provided name already exists
+  $(tmux has -t="$1" 2> /dev/null)
+  TMUX_SESSION_EXIST_CHECK="$?"
+
+  # If session with provided name doesn't already exist, create it
+  # (but wait for next command to attach)
+  if [[ "$TMUX_SESSION_EXIST_CHECK" != 0 ]]; then
+    tmux new -d -s "$1"
+  fi
+
+  # Attach to session with provided name
+  tmux -u $change -t "$1"
 }
 
 function __tmux-sessions() {
